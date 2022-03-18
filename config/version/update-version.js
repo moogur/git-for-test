556ae62ef2e7a2e89c6b6bfac3bc879d6de103df
Alexey Dvoretsky <moogur-mail@yandex.ru>
2022-03-18 21:25:17 +0300

    Feature/wbs 20 commit (#18)
    * [d
    
    * 2
    
    * a2
    
    * [WBS-20] Added commit
    
    * [WBS-20] Added commit
    
    * [WBS-20] Commit
    
    * [WBS-20] fix github hooks

---
+++ config/version/update-version.js
@@ -0,0 +1,13 @@
+// import * as fs from "fs";
+// import shell from "shelljs";
+
+// if (!shell.which("git")) {
+//   shell.echo("This script requires git");
+//   shell.exit(1);
+// }
+
+// async function updateVersion() {
+//   const version = JSON.parse(fs.readFileSync("./config/version/version.json", "utf8"));
+// }
+
+// updateVersion();
